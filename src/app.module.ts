0d1658925b2d56cc539fb392407f8f8855d4a011
Rafael Nunes <rafaelnfsq@gmail.com>
2023-02-17 12:42:55 -0300

    feat: add config module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CustomersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
