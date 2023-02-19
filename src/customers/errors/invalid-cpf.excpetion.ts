e838f2303bc8e04b0274ef0615c20985b6f54aff
Rafael Nunes <rafaelnfssq@gmail.com>
2023-02-19 10:19:19 -0300

    fix: fix the http response

--- src/customers/errors/invalid-cpf.excpetion.ts
+++ src/customers/errors/invalid-cpf.excpetion.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 export class InvalidCpfException extends HttpException {
   constructor() {
-    super('Invalid CPF!', HttpStatus.BAD_REQUEST);
+    super('Invalid CPF!', HttpStatus.UNPROCESSABLE_ENTITY);
   }
 }
